f488261dc4b9d74bc9e5eb799aa1619124d44d00
Zoe Ames <zoeames@gmail.com>
2018-03-27 18:49:48 -0500

    added class for carnivore or vegetable

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const printToDom = (domString, divId) => {
 const buildDomString = (fancyArray) => {
   let domString = "";
   fancyArray.forEach((animal) => {
-    domString += `<div class="animal">`;
+    if(animal.isCarnivore){
+      domString += `<div class="animal carnivore">`;
+    } else {
+      domString += `<div class="animal vegetable">`;
+    }
     domString +=   `<h1>${animal.name}</h1>`;
     domString +=   `<h3>${animal.number}</h3>`;
     domString +=   `<img class="animal-image" src="${animal.imageUrl}">`;
